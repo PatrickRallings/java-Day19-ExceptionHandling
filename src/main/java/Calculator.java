42bf55c96de108607a86b6e62001e51f9e87c224
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-26 09:10:59 -0400

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+
+    public int factorial(int num) throws IllegalArgumentException {
+        if (num < 0) {
+            throw new IllegalArgumentException("Number cannot be negative.");
+        } else {
+            int answer = 1;
+            for (int i = 1; i <= num; i++) {
+                answer *= i;
+            }
+            return answer;
+        }
+    }
+
+    public int binomialCoefficent(int setSize, int subsetSize) {
+        if (subsetSize < 0 || setSize < subsetSize) {
+            throw new IllegalArgumentException("Invalid arguments entered.");
+        } else {
+            int numerator = factorial(setSize);
+            int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
+            return numerator / denominator;
+        }
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) throws IllegalArgumentException{
+        if (name == null || name.trim().isEmpty() || name.length() > 40){
+            throw new IllegalArgumentException("Name is not valid.");
+        }
+        if (age < 0 || age > 120){
+            throw new IllegalArgumentException("Age entered is out of acceptable range.");
+        } else {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
